f33d33d97f1cf04216e56a0e8e3e911965873b31
Victor Ngaboyamahoro <nvichack@gmail.com>
2022-08-16 01:55:49 +0200

    Updated model fields, reformatted serializer, fixed manager to only accept relevant data when creating user with command line

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, Group
 import uuid
 
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django_countries.fields import CountryField
 from phonenumber_field.modelfields import PhoneNumberField
@@ -51,7 +51,7 @@ class User(AbstractUser):
         swappable = 'AUTH_USER_MODEL'
 
     def __str__(self):
-        return self.phone_number
+        return str(self.phone_number)
 
 
 class Verification(models.Model):
@@ -60,6 +60,12 @@ class Verification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_valid = models.BooleanField(default=True)
     is_used = models.BooleanField(default=False)
+    channels = [
+        ("PHONE_NUMBER", "PHONE_NUMBER"),
+        ("EMAIL", "EMAIL"),
+        ("ALL", "ALL"),
+    ]
+    channel = models.CharField(max_length=30, default="ALL", choices=channels)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework.serializers import ModelSerializer
 from .models import User, Verification
 from django.contrib.auth.models import Group
@@ -10,7 +11,6 @@ class GroupMiniSerializer(ModelSerializer):
 
 
 class UserMiniSerializer(ModelSerializer):
-
     class Meta:
         model = User
         fields = [
@@ -19,6 +19,7 @@ class UserMiniSerializer(ModelSerializer):
             "last_name",
             "phone_number",
             "email",
+            "is_email_verified",
             "nationality",
             "is_active",
             "is_staff",
@@ -28,11 +29,25 @@ class UserMiniSerializer(ModelSerializer):
             "verification_status",
             "profile_photo"
         ]
-        read_only_fields = ['is_active', 'is_staff', 'verification_status']
+        read_only_fields = ['is_active', 'is_staff', 'verification_status', 'is_email_verified']
 
     def to_representation(self, instance):
         serialized_data = super(UserMiniSerializer, self).to_representation(instance)
+        serialized_data['nationality'] = instance.nationality.name
+
+        if instance.birthdate:
+            now = timezone.now().date()
+            td = now - instance.birthdate
+            years = int(td.days / 365)
+            serialized_data['age'] = f"{years} Years"
+        else:
+            serialized_data['age'] = None
         return serialized_data
+
+    def update(self, instance, validated_data):
+        if validated_data.get("email") and instance.email and validated_data.get("email") != instance.email:
+            validated_data["is_email_verified"] = False
+        return super(UserMiniSerializer, self).update(instance, validated_data)
 
 
 class VerificationSerializer(ModelSerializer):
